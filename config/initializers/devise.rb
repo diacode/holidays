e4da714b7fa367e55d3b166ac92c12de9781ff49
Víctor Viruete <victor@diacode.com>
2015-04-14 16:10:30 +0200

    bootstrapping project

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,4 +256,10 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+  # Devise Layouts
+  Rails.application.config.to_prepare do
+    Devise::SessionsController.layout "devise"
+    Devise::PasswordsController.layout "devise"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: 'admin@diacode.com', password: '12345678', first_name: 'admin', last_name: 'istrator', admin: true)
+User.create(email: 'user@diacode.com', password: '12345678', first_name: 'regular', last_name: 'user')
