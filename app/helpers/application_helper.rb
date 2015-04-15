04f08df496c65312ca8314fcac5d60670e9b3431
Ricardo <bigardone@gmail.com>
2015-04-15 09:10:34 +0200

    Helper methods for avatar, font families and js class names

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def gravatar_url(email, size = 80)
+    gravatar_id = Digest::MD5::hexdigest(email).downcase
+    url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
+
+  def google_fonts
+    [
+      "Open+Sans:400italic,400,600,700,800"
+    ].join '|'
+  end
+
+  def js_view_name
+    "#{controller_name}_#{action_name}"
+  end
 end
