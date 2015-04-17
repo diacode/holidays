96325726d547ca3296ecafea82fc9022c9501c3c
Ricardo <bigardone@gmail.com>
2015-04-17 18:48:46 +0200

    Added gon globals

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   before_filter :authenticate_user!
+  before_action :set_gon_globals
+
+  private
+
+  def set_gon_globals
+    gon.authenticity_token = form_authenticity_token
+    gon.locale = I18n.locale
+  end
 end
