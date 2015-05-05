82a8ee4c94d49031542d76d118140da9e4fda462
Ricardo <bigardone@gmail.com>
2015-05-05 16:30:55 +0200

    Added new routes for editing vacations requests

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   root to: 'dashboard#index'
   resources :users, path: 'team'
 
-  resources :vacation_requests, only: [:index]
+  resources :vacation_requests, only: [:index, :edit]
 
   namespace :api do
     namespace :v1 do
-      resources :vacation_requests, only: [:index, :create, :update, :destroy] do
+      resources :vacation_requests, only: [:index, :show, :create, :update, :destroy] do
         member do
           put :approve
           put :reject
