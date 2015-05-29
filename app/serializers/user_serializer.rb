aed9e6554ee5e441d12e1ab8cbe61d6da4925183
Ricardo <bigardone@gmail.com>
2015-05-29 10:32:38 +0200

    Added new attributes and methods to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,5 +2,18 @@ class UserSerializer < ActiveModel::Serializer
   attributes  :id,
               :email,
               :admin,
-              :full_name
+              :full_name,
+              :on_holidays,
+              :available_days,
+              :current_year_days_spent,
+              :avatar_url
+
+  def on_holidays
+    object.on_holidays?
+  end
+
+  def avatar_url
+    gravatar_id = Digest::MD5::hexdigest(object.email).downcase
+    "https://gravatar.com/avatar/#{gravatar_id}.png?s=100"
+  end
 end
