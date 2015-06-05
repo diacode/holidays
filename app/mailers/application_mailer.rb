e191a065931c6423ecf53afcb277ab2bb8d93f7a
Ricardo <bigardone@gmail.com>
2015-06-05 12:02:02 +0200

    Added custom configuration initializer with default from email address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'holidays@diacode.com'
+  default from: Rails.configuration.x.default_from_email_address
   layout 'mailer'
 end
---
+++ config/initializers/custom_configuration.rb
@@ -0,0 +1,2 @@
+# Default email address to be used as from by mailers
+DiacodeHolidays::Application.config.x.default_from_email_address = 'holidays@diacode.com'
