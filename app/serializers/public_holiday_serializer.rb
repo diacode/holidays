9ea5ca933a00152d851b6ef72aca0271e664f267
Victor Viruete <hopsor@gmail.com>
2016-11-07 17:24:30 +0100

    This was missing

--- app/serializers/public_holiday_serializer.rb
+++ app/serializers/public_holiday_serializer.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: public_holidays
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  day        :date
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_public_holidays_on_day  (day)
+#
+
 class PublicHolidaySerializer < ActiveModel::Serializer
   attributes :id, :name, :day
 end
